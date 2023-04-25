058fca4eb71f75ad49141e4635b258d5011d8210
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-04-25 17:49:59 +0900

    Update 달리기경주.py

--- 프로그래머스/레벨1/달리기경주.py
+++ 프로그래머스/레벨1/달리기경주.py
@@ -1,8 +1,8 @@
 def solution(players, callings):
     name_rank = dict()
-    for rank, name in enumerate(players):
+    for rank, name in enumerate(players):               # enumerate를 이용해서 순위와 이름을 dictionary에 저장
         name_rank[name] = rank
-    for call in callings:
+    for call in callings:                               # 불러진 이름의 순위와 올라간순위 저장하고, dictionary에서 순위를 바꾸고 players의 순서를 바꿔서 이름목록도 바
         now = name_rank[call]
         nxt = now-1
         name_rank[call] = nxt
