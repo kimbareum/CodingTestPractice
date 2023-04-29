c2fd8c93664f903cb6f7263ea59bc76e49107f12
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-04-29 20:29:18 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -1,3 +1,4 @@
 def solution(x):
-    suma = sum([int(i) for i in str(x)])
+    # suma = sum([int(i) for i in str(x)])
+    suma = sum(map(int,list(str(x))))
     return True if x%suma == 0 else False
