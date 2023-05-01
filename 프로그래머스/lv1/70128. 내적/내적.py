d59816ffca71c8254fc48105f2b9f58837493f1b
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-01 19:55:38 +0900

    Update 내적.py

--- 프로그래머스/lv1/70128. 내적/내적.py
+++ 프로그래머스/lv1/70128. 내적/내적.py
@@ -1,2 +1,6 @@
 def solution(a, b):
-    return sum(map(lambda x,y:x*y,a,b))
+    #return sum(map(lambda x,y:x*y,a,b))
+    answer = 0
+    for x,y in zip(a,b):
+        answer+=x*y
+    return answer
