c8747cb84ebb31326f3eeb7ead46b4ce81da18d6
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-01 20:06:26 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.25 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,21 @@
+def divisor(num):
+    if num==1:
+        return -1
+    cnt = 2
+    for i in range(2,num//2+1):
+        if num%i ==0:
+            cnt+=1
+    if cnt%2 == 0:
+        return num
+    else:
+        return -num
+
+
+def solution(left, right):
+    answer=0
+    for i in range(left,right+1):
+        answer += divisor(i)
+    return answer
+        
+        
+        
