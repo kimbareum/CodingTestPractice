60a76916106e50998a2a7747af0ec56aef155781
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-05 21:29:59 +0900

    [level 1] Title: 폰켓몬, Time: 0.38 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
 def solution(nums):
-    select = len(nums)//2
-    set_pokemon = set(nums)
-    if len(set_pokemon) <= select:
-        return len(set_pokemon)
-    else:
-        return select
+    return min(len(set(nums)),len(nums)//2)
