6ea58f49d45cf5d4f68f098dbeb337e2c6f2b97e
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-05 22:15:31 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 9.92 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+def isprime(num):
+    for i in range(2,num//2+1):
+        if num%i == 0:
+            return False
+    return True
+
+def solution(nums):
+    answer = 0
+    nums2 = [sum(i) for i in combinations(nums,3)]
+    for i in nums2:
+        if isprime(i):
+            answer += 1
+    return answer
