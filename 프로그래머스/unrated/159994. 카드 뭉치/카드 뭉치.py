00d0e29fbee4723c2a8b0aee2b85109b89c7ae92
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-06 18:54:36 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,10 +1,10 @@
 def solution(cards1, cards2, goal):
     i, j, k = 0, 0, 0
     while k<len(goal):
-        if i != len(cards1) and goal[k] == cards1[i]:
+        if i < len(cards1) and goal[k] == cards1[i]:
             i += 1
             k += 1
-        elif j != len(cards2) and goal[k] == cards2[j]:
+        elif j < len(cards2) and goal[k] == cards2[j]:
             j += 1
             k += 1
         else:
