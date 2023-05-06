1630a25579f77d589f5ed64182ef04e408678e93
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-06 17:30:02 +0900

    [unrated] Title: 과일 장수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,8 +1,10 @@
 def solution(k, m, score):
     if len(score) < m:
         return 0
-    answer = 0
+    
     score.sort()
-    for i in range(1,len(score)//m+1):
-        answer += score[-m*i]*m
-    return answer
+    # answer = 0
+    # for i in range(1,len(score)//m+1):
+    #     answer += score[-m*i]*m
+    # return answer
+    return sum(score[len(score)%m::m])*m
