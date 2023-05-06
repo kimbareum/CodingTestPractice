a54c3edb0caa99ef1efb928a3b1eb4ccd40e5add
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-06 18:06:40 +0900

    Update 실패율.py

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,10 +1,31 @@
+# def solution(N, stages):
+#     answer = dict()
+#     reach = len(stages)
+#     for i in range(1,N+1):
+#         if reach == 0:
+#             answer.update({i : 0})
+#         else:
+#             answer.update({i : stages.count(i)/reach})
+#             reach -= stages.count(i)
+#     return sorted(answer, key = answer.get, reverse = True)
+
+# 매번 count 해주는것보단 collections 의 Counter를 사용해서 정리해두면 빠름.
+
+from collections import Counter
+
 def solution(N, stages):
     answer = dict()
     reach = len(stages)
+    fail = Counter(stages)
+    fail = dict(sorted(list(fail.items())))
     for i in range(1,N+1):
+        temp = fail.get(i)
         if reach == 0:
             answer.update({i : 0})
         else:
-            answer.update({i : stages.count(i)/reach})
-            reach -= stages.count(i)
+            if temp == None:
+                answer.update({i : 0})
+            else :
+                answer.update({i : temp/reach})
+                reach -= temp
     return sorted(answer, key = answer.get, reverse = True)
