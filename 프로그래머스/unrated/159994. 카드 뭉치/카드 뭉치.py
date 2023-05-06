a37f82eddc797ec13d2adb343c1cfc03f19c5f3d
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-06 18:53:59 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,12 @@
+def solution(cards1, cards2, goal):
+    i, j, k = 0, 0, 0
+    while k<len(goal):
+        if i != len(cards1) and goal[k] == cards1[i]:
+            i += 1
+            k += 1
+        elif j != len(cards2) and goal[k] == cards2[j]:
+            j += 1
+            k += 1
+        else:
+            return 'No' 
+    return 'Yes'
