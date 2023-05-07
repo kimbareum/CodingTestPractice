bab04095f0e6d48a75572a4199bcc61f2813d1bb
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-07 15:58:14 +0900

    [unrated] Title: 문자열 나누기, Time: 0.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -1,17 +1,11 @@
 def solution(s):
-    s = list(reversed(s))
-    answer = 1
-    while len(s)>2:
-        char = s.pop()
-        equal, notEqual = 1, 0 
-        while equal != notEqual and len(s) > 0:
-            temp = s.pop()
-            if char == temp:
-                equal += 1
-            else:
-                notEqual += 1
-        if len(s) == 0:
-            break
-        if equal == notEqual:
-            answer += 1
+    equal, notEqual, answer = 0, 0, 0
+    for i in s:
+        if equal==notEqual:
+            answer+=1
+            a=i
+        if i==a:
+            equal += 1
+        else:
+            notEqual += 1
     return answer
