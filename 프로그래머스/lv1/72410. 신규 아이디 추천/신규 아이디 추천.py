e6f383d923bec459ac0cd1b7de84952163e155ea
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-07 16:51:14 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -4,11 +4,18 @@ def solution(new_id):
     new_id = new_id.lower()
     new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
     new_id = re.sub('\.+','.', new_id)
+    
+    # st = re.sub('^[.]|[.]$', '', st)
     new_id = new_id.strip('.')
+    
+    # st = 'a' if len(st) == 0 else st[:15]
     if new_id == '':
         return 'aaa'
     if len(new_id) >= 16:
         new_id = new_id[0:15].rstrip('.')
+    
+    #st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
     while len(new_id) <= 2:
         new_id += new_id[-1]
+
     return new_id
