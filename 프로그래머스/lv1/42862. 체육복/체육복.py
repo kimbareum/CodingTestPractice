52ab04c6c6eb094c3a2c5deb3b1ade14aad53dbf
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-07 15:24:00 +0900

    Update 체육복.py

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -2,13 +2,13 @@ def solution(n, lost, reserve):
     reserve = set(reserve)
     lost = set(lost)
     lost, reserve = lost - reserve, reserve - lost
-    reserved = []
+    reserved = set()
     answer = n-len(lost)
     for i in lost:
         if i-1 not in reserved and i-1 in reserve:
             answer += 1
-            reserved.append(i-1)
+            reserved.add(i-1)
         elif i+1 not in reserved and i+1 in reserve:
             answer += 1
-            reserved.append(i+1)
+            reserved.add(i+1)
     return answer
