b27109eb7fd07f158d49266bc705b188e7e8be1b
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-07 15:37:54 +0900

    [unrated] Title: 옹알이 (2), Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    canSay = ["aya", "ye", "woo", "ma" ]
+    cantSay = ["ayaaya", "yeye", "woowoo", "mama" ]
+    answer = 0
+    for i in range(len(babbling)):
+        for j, k in zip(canSay, cantSay) :
+            if k in babbling[i]:
+                break
+            babbling[i] = babbling[i].replace(j," ")
+        if not babbling[i].strip() :
+            answer +=1
+    return answer
