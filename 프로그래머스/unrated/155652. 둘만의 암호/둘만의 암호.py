f9ec6f6512dc0062763880b13f0a0839580a5a3a
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-07 17:07:11 +0900

    [unrated] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -2,11 +2,8 @@ def solution(s, skip, index):
     alphabet = [i for i in 'abcdefghijklmnopqrstuvwxyz' if i not in skip]
     answer = ''
     for i in s:
-        if i in alphabet:
-            try:
-                answer += alphabet[alphabet.index(i)+index]
-            except:
-                answer += alphabet[(alphabet.index(i)+index)%len(alphabet)]
-        else:
-            answer += i  
+        try:
+            answer += alphabet[alphabet.index(i)+index]
+        except:
+            answer += alphabet[(alphabet.index(i)+index)%len(alphabet)]
     return answer
