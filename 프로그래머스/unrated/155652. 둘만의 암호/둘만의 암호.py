4774fd23af72e2303e21a4689fdb2471928c50c0
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-07 17:09:58 +0900

    Update 둘만의 암호.py

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,9 +1,7 @@
 def solution(s, skip, index):
     alphabet = [i for i in 'abcdefghijklmnopqrstuvwxyz' if i not in skip]
     answer = ''
+    lenAlphabet = len(alphabet)
     for i in s:
-        try:
-            answer += alphabet[alphabet.index(i)+index]
-        except:
-            answer += alphabet[(alphabet.index(i)+index)%len(alphabet)]
+        answer += alphabet[(alphabet.index(i)+index)%lenAlphabet]
     return answer
