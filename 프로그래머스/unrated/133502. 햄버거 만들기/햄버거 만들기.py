3e27252ee3d65802780c7b32a81051a624f8e585
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-07 18:57:17 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,12 +1,10 @@
-from collections import deque
 def solution(ingredient):
     if len(ingredient) < 4:
         return 0
-    ing = deque(ingredient)
     answer = 0
-    queue = [ing.popleft() for _ in range(3)]
-    while ing:
-        queue.append(ing.popleft())
+    queue = []
+    for i in ingredient:
+        queue.append(i)
         while len(queue) >=4 and queue[-4:] == [1, 2, 3, 1]:
             answer += 1
             for _ in range(4):
