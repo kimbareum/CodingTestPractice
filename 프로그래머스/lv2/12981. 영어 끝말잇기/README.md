6ac620c2dba60eb753ea8bf529c6805b4486eb8d
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-09 17:45:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.08 ms
+메모리: 10.2 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,16 +1,10 @@
 def solution(n, words):
     visited = []
-    fail_index = 0
-    for i, now in enumerate(words,1):
+    for i, now in enumerate(words):
         if not visited:
             pass
         elif now in visited or now[0] != visited[-1][-1]:
-            fail_index = i
-            break
+            return [(i%n)+1, (i//n)+1]
         visited.append(now)
-    if fail_index == 0:
-        return [0, 0]
     else:
-        person = fail_index%n if fail_index%n else n
-        times = fail_index//n if person == n else fail_index//n + 1
-        return [person , times]
+        return [0, 0]
