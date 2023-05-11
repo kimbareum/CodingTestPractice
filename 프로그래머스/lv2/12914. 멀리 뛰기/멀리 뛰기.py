719212391afda5327bcf2ba371a127ef1ed8ae0b
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-11 12:54:35 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,10 +1,8 @@
 def solution(n):
     if n <= 3:
         return n
-    prev1 = 2
-    prev2 = 1
-    answer = 0
-    for i in range(3,n+1):
-        answer = prev1 + prev2
-        prev1, prev2 = answer, prev1
-    return answer%1234567
+    prev1 = 3
+    prev2 = 2
+    for i in range(4,n+1):
+        prev1, prev2 = prev1 + prev2, prev1
+    return prev1%1234567
