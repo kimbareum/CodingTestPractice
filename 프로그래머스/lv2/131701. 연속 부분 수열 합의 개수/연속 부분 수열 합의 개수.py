634f6e7521babcf5c3be45c20ee8879904bf6b6d
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-12 11:37:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 361.05 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,7 +1,19 @@
+# def solution(elements):
+#     data = set()
+#     elements2 = elements[:] + elements[:]
+#     for i in range(1, len(elements) + 1):
+#         for j in range(len(elements)):
+#             data.add(sum(elements2[j:j+i]))
+#     return len(data)
+
 def solution(elements):
-    data = set()
-    elements2 = elements[:] + elements[:]
-    for i in range(1, len(elements) + 1):
-        for j in range(len(elements)):
-            data.add(sum(elements2[j:j+i]))
-    return len(data)
+    ll = len(elements)
+    res = set()
+
+    for i in range(ll):             # 처음꺼부터 마지막꺼까지 1개씩 순회
+        ssum = elements[i]          # 길이가 1인 경우를 추가
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]  # 2인경우부터 하나씩 추가
+            res.add(ssum)
+    return len(res)
