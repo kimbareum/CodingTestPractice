77b77009b067337ae1cd51e5a02e6da73f4b32bb
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-12 17:51:33 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.65 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,12 +1,12 @@
 def solution(arr1, arr2):
     arr2 = list(map(list, zip(*arr2)))
     answer =[]
-    for i in range(len(arr1)):
+    for i in arr1:
         temp = []
-        for j in range(len(arr2)):
+        for j in arr2:
             suma = 0
             for k in range(len(arr1[0])):
-                suma += arr1[i][k]*arr2[j][k]
+                suma += i[k]*j[k]
             temp.append(suma)
         answer.append(temp)           
     return answer
