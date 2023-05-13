8e096a340ee5d4c4a0f8c78306994aa7d5f05ce7
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-13 23:36:29 +0900

    기능개발 math.ceil 없이 반올림하는법 update

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -2,6 +2,7 @@ import math
 
 def solution(progresses, speeds):
     left_days = [math.ceil((100-progress)/speed) for progress, speed in zip(progresses,speeds)]
+    # -((progress-100)//speed) math.ceil 없이 올림하기
     answer = [0]
     left_day = left_days[0]
     for day in left_days:
