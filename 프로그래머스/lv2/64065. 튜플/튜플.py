e2d3b9c31bfe2808c78bc73993782b46e4f1288c
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-13 16:34:45 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,12 +1,18 @@
 import re
+from collections import Counter
 
 def solution(s):
-    p = re.compile('\{([^}|^{]+)')
-    s = p.findall(s)
-    s = sorted(s, key = len)
-    answer = []
-    for set_1 in s:
-        set_1 = set_1.split(',')
-        answer += [int(i) for i in set_1 if int(i) not in answer]
-    return answer
+#     p = re.compile('\{([^}|^{]+)')
+#     s = p.findall(s)
+#     s = sorted(s, key = len)
+#     answer = []
+#     for set_1 in s:
+#         set_1 = set_1.split(',')
+#         answer += [int(i) for i in set_1 if int(i) not in answer]
+#     return answer
+    
+    count = Counter(re.findall('\d+', s))
+    count = map(int, sorted(count, key = lambda x: count[x], reverse=True))
+    return list(count)
+    
     
