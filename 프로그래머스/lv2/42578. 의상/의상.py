a2f97e14129cc4c5938ae39f0a01470a85477807
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-13 14:56:28 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,13 +1,21 @@
+# def solution(clothes):
+#     answer = 1
+#     equipment = {}
+#     for item, kind in clothes:
+#         if not equipment.get(kind):
+#             equipment.update({kind:[item]})
+#         else:
+#             equipment[kind].append(item)
+            
+#     for items in equipment.values():
+#         answer *= len(items)+1   
+#     return answer-1
+
+from collections import Counter
+
 def solution(clothes):
     answer = 1
-    equipment = {}
-    
-    for i, j in clothes:
-        if not equipment.get(j):
-            equipment.update({j:[i]})
-        else:
-            equipment[j].append(i)
-            
-    for items in equipment.values():
-        answer *= len(items)+1   
-    return answer-1
+    count = Counter(map(lambda x: x[1], clothes))
+    for i in count.values():
+        answer *= i+1
+    return answer - 1
