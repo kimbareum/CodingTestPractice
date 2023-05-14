3035988ee466843028124205fe82d614989aa34e
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-14 12:10:08 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 4.56 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,19 +1,21 @@
-def isprime(num):
-    if num <= 1:
-        return 0
-    for i in range(2,int(num**0.5)+1):
-        if num%i == 0:
-            return 0
-    return 1
-
 def solution(n, k):
     answer = 0
+    #진법변환
     numbers = ''
     while n >= 1:
         rest = n % k
         n //= k
         numbers = str(rest) + numbers
+        
     numbers = [int(i) for i in numbers.split('0') if i]
+    
+    # 소수 판별
     for number in numbers:
-        answer += isprime(number)
+        if number <= 1:
+            continue
+        for i in range(2,int(number**0.5)+1):
+            if number%i == 0:
+                break
+        else:answer += 1
+        
     return answer
