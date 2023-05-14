fea6d81f2d2f58f18f95789a4b5d36c2b76588ec
kimbareum <right.kim93@gmail.com>
2023-05-14 10:00:39 +0900

    프로세스 방법 수정

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -11,3 +11,26 @@ def solution(priorities, location):
         else:
             priority_map.popleft()
             answer += 1
+
+# index(max) 를 매번 넣는게 생각보다 느릴지도.
+
+# def solution(priorities, location):
+#     answer = 0
+#     priorities = deque(priorities)
+#     top_priority = max(priorities)
+#     while True:
+#         curr = priorities.popleft()
+#         if top_priority == curr:
+#             answer += 1
+#             if location == 0:
+#                 break
+#             else:
+#                 location -= 1
+#             top_priority = max(priorities)
+#         else:
+#             priorities.append(curr)
+#             if location == 0:
+#                 location = len(priorities)-1
+#             else:
+#                 location -= 1
+#     return answer
