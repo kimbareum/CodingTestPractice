79351918c6304bbc64ee5ed9b3eeda89f5f89afc
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-17 16:22:04 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -3,6 +3,6 @@ import re
 def solution(babbling):
     babble = ["aya", "ye", "woo", "ma"]
     for i in range(len(babbling)):
-        babbling[i] = re.sub('(aya|ye|woo|ma)', ' ',babbling[i]).strip()
+        babbling[i] = re.sub('(aya|ye|woo|ma)', '',babbling[i])
     
     return len([i for i in babbling if not i])
