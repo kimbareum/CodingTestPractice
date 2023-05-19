e4259fc30bffb5cd57af5be270b70cdb826814c6
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-19 12:24:42 +0900

    [unrated] Title: 숫자 변환하기, Time: 47.37 ms, Memory: 17.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def solution(x, y, n):    
+    if x==y:
+        return 0
+    queue = deque([(x,0)])
+    answer = [0 for _ in range(y+1)]
+    while queue:
+        curr, count = queue.popleft()
+        count += 1
+        if curr+n <= y and answer[curr+n] == 0:
+            answer[curr+n] = count
+            queue.append((curr+n, answer[curr+n]))
+        if curr*2 <= y:
+            if answer[curr*2] == 0:
+                answer[curr*2] = count
+                queue.append((curr*2, answer[curr*2]))
+            else:
+                answer[curr*2] = min(count,answer[curr*2])
+                queue.append((curr*2, answer[curr*2]))
+        if curr*3 <= y:
+            if answer[curr*3] == 0:
+                answer[curr*3] = count
+                queue.append((curr*3, answer[curr*3]))
+            else:
+                answer[curr*3] = min(count,answer[curr*3])
+                queue.append((curr*3, answer[curr*3]))
+    return answer[-1] if answer[-1] else -1
