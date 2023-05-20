64f88601c08d19c4f8150a9a0a2a9ff2f3a2b37b
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-20 23:27:31 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 67.52 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -6,32 +6,25 @@ def solution(arr):
     queue = deque([[0, 0, arr_len-1, arr_len-1]])
     while queue:
         y1, x1, y2, x2 = queue.popleft()
-        temp = []
-        for row in range(y1,y2+1):
-            temp.extend(arr[row][x1:x2+1])
-        if all(temp):
-            for y in range(y1,y2+1):
-                for x in range(x1,x2+1):
-                    if x == x1 and y == y1:
-                        continue
-                    arr[y][x] = -1
+        if x1 == x2 and y1 == y2:
+            answer[arr[y1][x1]] += 1
             continue
-        elif any(temp):
-            pass
+        first = arr[y1][x1]
+        break_flag = 0
+        for row in range(y1,y2+1):
+            for col in range(x1,x2+1):
+                if arr[row][col] != first:
+                    break_flag = 1
+                    break
+            if break_flag == 1:
+                break
         else:
-            for y in range(y1,y2+1):
-                for x in range(x1,x2+1):
-                    if x == x1 and y == y1:
-                        continue
-                    arr[y][x] = -1
-            continue 
+            answer[first] += 1
+            continue
         ymid = y1+(y2-y1)//2
         xmid = x1+(x2-x1)//2
         queue.append([y1, x1, ymid, xmid])
         queue.append([y1, xmid+1, ymid, x2])
         queue.append([ymid+1, x1, y2, xmid])
         queue.append([ymid+1, xmid+1, y2, x2])
-    for row in arr:
-        answer[0] += row.count(0)
-        answer[1] += row.count(1)
     return answer
