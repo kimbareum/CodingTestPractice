e88674b8d88715c3f65676b54c616fc03c506dfd
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-20 09:59:12 +0900

    [level 2] Title: 소수 찾기, Time: 7.65 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,17 +1,29 @@
-from itertools import permutations
+# from itertools import permutations
 
-def solution(numbers):
-    cases = []
-    for i in range(1, len(numbers)+1):
-        cases.extend(permutations(numbers,i)) 
-    cases = set([int(''.join(i)) for i in cases])
-    answer = 0
-    for num in cases:
-        if num < 2:
-            continue
-        for i in range(2,int(num**0.5)+1):
-            if num%i == 0:
-                break
-        else:
-            answer += 1 
-    return answer
+# def solution(numbers):
+#     cases = []
+#     for i in range(1, len(numbers)+1):
+#         cases.extend(permutations(numbers,i)) 
+#     cases = set([int(''.join(i)) for i in cases])
+#     answer = 0
+#     for num in cases:
+#         if num < 2:
+#             continue
+#         for i in range(2,int(num**0.5)+1):
+#             if num%i == 0:
+#                 break
+#         else:
+#             answer += 1 
+#     return answer
+
+from itertools import permutations
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        if not a:
+            break
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
