e4e2450f42b8b80b93fa5d8650be2db333aaf794
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-21 11:49:34 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,12 +1,12 @@
 def solution(n):
     answer = ''
-    while n >= 1:
+    while n > 0:
+        n -= 1
         n, rest = divmod(n,3)
         if rest == 0:
-            answer = '4'+ answer
-            n -= 1
+            answer = '1'+ answer
         elif rest == 1:
-            answer = '1' + answer
+            answer = '2' + answer
         else:
-            answer = '2' + answer      
+            answer = '4' + answer      
     return answer
