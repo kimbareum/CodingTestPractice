e492e0917007cb885c2d0dfcaa3144d63535044a
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-22 22:36:47 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.67 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -5,15 +5,19 @@ def solution(m, musicinfos):
     m = p.findall(m)
     for string in musicinfos:
         start, end, name, melody = string.split(',')
+        # 멜로디를 #을 포함해서 분해
         melody = p.findall(melody)
-        
+        # 시간계산
         start = int(start[:2])*60 + int(start[-2:])
         time = int(end[:2])*60+int(end[-2:]) - start
-        
+        # 시간만큼 멜로디를 늘려줘서 재생된 멜로디로 만듬
         melody = ([melody[i%len(melody)] for i in range(time)])
+        # 재생된 멜로디안에 기억한 멜로디가 있는지 확인
         for i in range(len(melody)-len(m)+1):
             if melody[i:i+len(m)] == m:
                 answer.append((time, -start, name))
+                break
+    # 튜플같은곳에서 0번 인덱스값이 같으면 1번 인덱스값 기준으로 정렬함.
     answer.sort(reverse = True)
     return answer[0][2] if answer else "(None)"
             
