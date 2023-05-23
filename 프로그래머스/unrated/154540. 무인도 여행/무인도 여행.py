a59fb8f2ee26c3b7c1fa125b1cefc9c61a076106
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-23 17:48:45 +0900

    [unrated] Title: 무인도 여행, Time: 0.25 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -1,33 +1,31 @@
 def solution(maps):
     answer = []
-    stack = []
+
     stack2 = []
     visited = set()
-    for row in range(len(maps)):
-        for col in range(len(maps[0])):
+    rows, cols = len(maps), len(maps[0])
+    for row in range(rows):
+        for col in range(cols):
+            # 'X' 가 아닌 위치 찾기
             if maps[row][col] != 'X':
-                stack.append((row,col))
-    while stack:
-        if stack[-1] in visited:
-            stack.pop()
-            continue
-        stack2.append(stack.pop())
-        temp = 0
-        while stack2:
-            curr = stack2.pop()
-            if curr not in visited:
-                data = maps[curr[0]][curr[1]]
-                if data != 'X':
-                    temp += int(data)
-                    if 0 <= curr[0]-1 and (curr[0]-1, curr[1]) not in visited:
-                        stack2.append((curr[0]-1, curr[1]))
-                    if 0 <= curr[1]-1 and (curr[0], curr[1]-1) not in visited:
-                        stack2.append((curr[0], curr[1]-1))
-                    if curr[0]+1 < len(maps) and (curr[0]+1, curr[1]) not in visited:
-                        stack2.append((curr[0]+1, curr[1]))
-                    if curr[1]+1 < len(maps[0]) and (curr[0], curr[1]+1) not in visited:
-                        stack2.append((curr[0], curr[1]+1))
-                visited.add(curr)
-        if temp:
-            answer.append(temp)
+                stack = [(row,col)]
+                # 이미 방문했다면 탐색하지않음
+                if stack[-1] in visited:
+                    stack.pop()
+                    continue
+                # 방문하지 않았다면 주변 탐색
+                temp = 0
+                while stack:
+                    curr = stack.pop()
+                    if curr not in visited:
+                        data = maps[curr[0]][curr[1]]
+                        if data != 'X':
+                            temp += int(data)
+                            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                                new_row, new_col = curr[0] + dx, curr[1] + dy
+                                if 0 <= new_row < rows and 0 <= new_col < cols and (new_row, new_col) not in visited:
+                                    stack.append((new_row, new_col))
+                        visited.add(curr)
+                if temp:
+                    answer.append(temp)
     return sorted(answer) if answer else [-1]
