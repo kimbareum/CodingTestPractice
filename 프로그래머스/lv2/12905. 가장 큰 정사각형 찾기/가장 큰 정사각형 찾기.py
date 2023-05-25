ea690a2a35314d1924dc2a9de9532db2d4599db6
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-25 13:03:14 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 515.79 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -0,0 +1,13 @@
+def solution(board):
+    answer = 0
+    width, height = len(board[0]), len(board)
+    lengths = [[0 for _ in range(width+1)] for i in range(height+1)]
+    for row in range(1,height+1):
+        for col in range(1, width+1):
+            if board[row-1][col-1] == 1:
+                lengths[row][col] = 1+ min(lengths[row-1][col],
+                                           lengths[row][col-1],
+                                           lengths[row-1][col-1])
+            if lengths[row][col] > answer:
+                answer = lengths[row][col]
+    return answer ** 2
