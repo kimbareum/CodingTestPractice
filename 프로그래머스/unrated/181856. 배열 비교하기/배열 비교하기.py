11f92f97ffab0ee6e903e0fafae8371b5ec5503f
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-10-22 19:40:55 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    if len(arr1) > len(arr2):
+        return 1
+    elif len(arr2) > len(arr1):
+        return -1
+    else:
+        if sum(arr1) > sum(arr2):
+            return 1
+        elif sum(arr2) > sum(arr1):
+            return -1
+    return 0
